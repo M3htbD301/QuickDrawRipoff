022d701836810b11d035bae44ec2741c3d4e371a
M3htbD301 <81451074+M3htbD301@users.noreply.github.com>
2021-11-16 15:38:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+random_no = Math.floor((Math.random()*array_1.length)+1);
+
+console.log(random_no);
+sketch="";
+
+document.getElementById(base_sketch),innerHTML=sketch;
+
+timer_counter="0"
+timer_check=""
+drawn_sketch=""
+answer_holder=""
+score="0"
